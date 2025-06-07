819e90b1bba4e94533e13e4bf0b5f0fb89ff5a0a
SwiftDevOp <adebanjiimmanuel@gmail.com>
2025-06-07 22:41:22 +0100

    created a safeArea Widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  // ignore: unused_field
+  late double _deviceHeight, _deviceWidth;
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Pages')),
-      body: Center(
-        child: Text(
-          'Welcome to the Home Pages!',
-          style: TextStyle(fontSize: 24, color: Colors.white),
+      body: SafeArea(
+        child: Container(
+          color: Colors.red,
+          height: _deviceHeight,
+          width: _deviceWidth,
+          child: _pageTitle(),
         ),
       ),
-      backgroundColor: Colors.black,
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "GoMoon",
+      style: TextStyle(
+        fontSize: 32,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  // ignore: unused_element
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.red,
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          // ignore: unnecessary_string_escapes
+          image: AssetImage("assets\images\astro_moon.png"),
+        ),
+      ),
     );
   }
 }
